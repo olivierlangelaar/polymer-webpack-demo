683acd77291ee7234695624e01d7dfaf11d3f1c6
OlivierLangelaar <o.langelaar@gmail.com>
2017-09-27 13:25:26 +0200

    add iron ajax and app-metadata

--- src/components/example-app.js
+++ src/components/example-app.js
@@ -2,11 +2,19 @@ Polymer({
   is: 'example-app',
   properties: {
     route: Object,
+    jsonldData: {
+        type: String,
+        observer: 'setJSONLDFromBackend'
+    }
   },
   second() {
     this.set('routeData.page', 'second');
   },
   home() {
     this.set('routeData.page', '');
+  },
+  setJSONLDFromBackend: function(jsonldData) {
+      var script = document.getElementById('json-ld-script');
+      script.text = jsonldData;
   },
 });
